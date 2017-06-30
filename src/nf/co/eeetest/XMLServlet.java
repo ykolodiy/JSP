555cf3145ca78dd721226be05f87c37b3b73f1b3
Kolodiy <VKOLOYU@PEROOT.COM>
2017-06-30 11:21:23 -0500

    8

---
+++ src/nf/co/eeetest/XMLServlet.java
@@ -0,0 +1,28 @@
+package nf.co.eeetest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XMLServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		System.out.println("hello from get method XML");
+		PrintWriter writer = response.getWriter();
+	writer.println("<h3>nah XML</h3>");
+	
+		
+	
+	
+}
+	
+}
