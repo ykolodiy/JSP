dbcc918ce460f3165a8916cb4abce4051b265270
Kolodiy <VKOLOYU@PEROOT.COM>
2017-06-30 10:52:21 -0500

    dfsdfsdfsdf :quii

---
+++ src/nf/co/eeetest/mysuperpuperservlet.java
@@ -0,0 +1,34 @@
+package nf.co.eeetest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/m")
+public class mysuperpuperservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		System.out.println("hello from get method");
+		PrintWriter writer = response.getWriter();
+	writer.println("<h3>nah</h3>");
+	
+		
+	
+	
+	
+	
+	}
+
+}
